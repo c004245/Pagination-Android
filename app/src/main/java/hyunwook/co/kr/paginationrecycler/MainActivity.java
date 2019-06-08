db8762a19dc6c300fc5446874a6a56ab1f40e953
hyunwook <c004245@naver.com>
2019-06-08 13:15:39 +0900

    MIgrate to AndroidX

--- app/src/main/java/hyunwook/co/kr/paginationrecycler/MainActivity.java
+++ app/src/main/java/hyunwook/co/kr/paginationrecycler/MainActivity.java
@@ -1,6 +1,6 @@
 package hyunwook.co.kr.paginationrecycler;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
